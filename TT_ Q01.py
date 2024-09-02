c62020ac640f2fa06c2d3871465e593ca3d184ba
Turbat Turkhuu <20136824@tafe.wa.edu.au>
2024-09-02 18:04:21 +0800

    bug fix 1

--- TT_ Q01.py
+++ TT_ Q01.py
@@ -3,50 +3,50 @@
 
 # Constant
 MAGIC_NUMBER = 42
+MIN_RANGE = 0
+MAX_RANGE = 100
+
+
 # Variables
 
 # Function to validate number
-def validate_number(number):
+def validate_number(number_to_validate):
     try:
-        number = int(number)
-        return number
+        number_to_validate = int(number_to_validate)
+        return number_to_validate
     except:
         print("Your input has not been accepted, please enter a valid input. ")
         return 'No'
 
+
 # Determine the message based on the number entered
-def determine_clue(number_entered, MAGIC_NUMBER):
-    if number < MAGIC_NUMBER:
-        message = 'Number is too small'
-    elif number > MAGIC_NUMBER:
+def determine_clue(number_guessed, secret_number):
+    message = 'Yes, 42 is the magic number'
+    if number_guessed > secret_number:
         message = 'Number is too large'
-    elif number == MAGIC_NUMBER:
-        message = 'Yes, 42 is the magic number'
-    elif number == 'No':
+    elif number_guessed == 'No':
         message = 'Please enter a number'
-    else:
+    elif number_guessed < secret_number:
+        message = 'Number is too small'
+    elif number_guessed > MAX_RANGE or number_guessed < MIN_RANGE:
         message = 'Number out of boundary'
     return message
 
+
+number = 0
 # Main program
-number = input("Guess the magic number : (1-99) ")
-if number.islower() == 'quit':
-    {
-        print('Sad to see that you have given up');
-        quit();
-
-    }
-else:
-    {
-    number = validate_number(number);
-    clue = determine_clue(number, MAGIC_NUMBER);
-    print(clue);
-
-    }
-
+while number != MAGIC_NUMBER:
+    number = input("Guess the magic number : (1-99) ")
+    number = number.lower()
+    if number == 'quit':
+        print('Sad to see that you have given up')
+        quit()
+    number = validate_number(number)
+    clue = determine_clue(number, MAGIC_NUMBER)
+    print(clue)
 
 # Input from the user
 
-#validate_number(number)
-#print(determine_clue(0,42))
-#print("The end")
+# validate_number(number)
+# print(determine_clue(0,42))
+# print("The end")
